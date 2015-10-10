1797d1ecabd2c978f3c474178ef2f178e2e6d5d8
Gustavo <gustavo.m.netto@outlook.com>
2015-10-10 11:59:32 -0400

    AI working but problem with allow just one button at time

--- Tic-Tac-Toe/Tic-Tac-Toe.ino
+++ Tic-Tac-Toe/Tic-Tac-Toe.ino
@@ -31,22 +31,44 @@
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin_1 = 2;    // the number of the pushbutton pin_1
-const int buttonPin_2 = 3;    // the number of the pushbutton pin_2
+const int buttonPin_0 = 2;    // the number of the pushbutton pin_1
+const int buttonPin_1 = 3;    // the number of the pushbutton pin_2
+const int buttonPin_2 = 4;    // the number of the pushbutton pin_1
+const int buttonPin_3 = 5;    // the number of the pushbutton pin_2
+const int buttonPin_4 = 6;    // the number of the pushbutton pin_1
+const int buttonPin_5 = 7;    // the number of the pushbutton pin_2
+const int buttonPin_6 = 8;    // the number of the pushbutton pin_1
+const int buttonPin_7 = 9;    // the number of the pushbutton pin_2
+const int buttonPin_8 = 10;    // the number of the pushbutton pin_1
+
+
 const int ledPin_1 = 13;      // the number of the LED pin_1
 const int ledPin_2 = 12;      // the number of the LED pin_2
-const int choose_order_pin = 4; //The number of the choose order button
+const int choose_order_pin = 11; //The number of the choose order button
 
 // Variables will change:
-int ledState_1 = LOW;         // the current state of the output pin
-int buttonState_1;             // the current reading from the input pin
-int lastButtonState_1 = LOW;   // the previous reading from the input pin
+int buttonState_0 = HIGH;
+int buttonState_1 = HIGH;    
+int buttonState_2 = HIGH;    
+int buttonState_3 = HIGH;    
+int buttonState_4 = HIGH;    
+int buttonState_5 = HIGH;    
+int buttonState_6 = HIGH;
+int buttonState_7 = HIGH;
+int buttonState_8 = HIGH;  
 
-int ledState_2 = LOW;         // the current state of the output pin
-int buttonState_2;             // the current reading from the input pin
-int lastButtonState_2 = LOW;   // the previous reading from the input pin
+                      
+int lastButtonState_0 = LOW; 
+int lastButtonState_1 = LOW; 
+int lastButtonState_2 = LOW; 
+int lastButtonState_3 = LOW; 
+int lastButtonState_4 = LOW; 
+int lastButtonState_5 = LOW; 
+int lastButtonState_6 = LOW; 
+int lastButtonState_7 = LOW; 
+int lastButtonState_8 = LOW; 
 
-int order_button_state;
+int order_button_state = HIGH;
 int last_order_state = LOW;
 
 int order = 0; //indicates if the order was chosen
@@ -64,16 +86,30 @@ Pos create_position(int x, int y) {
 
 int board[][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
+int const buttonPin_to_board[][2] = {{0,0}, {0, 1}, {0,2}, {1, 0},{1,1}, {1, 2}, {2,0}, {2, 1}, {2,2}, {3, 0}};
+
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long lastDebounceTime = 0;  // the last time the output pin was toggled
+long lastDebounceTime_0 = 0;
+long lastDebounceTime_1 = 0;
+long lastDebounceTime_2 = 0;
+long lastDebounceTime_3 = 0;
+long lastDebounceTime_4 = 0;
+long lastDebounceTime_5 = 0;
+long lastDebounceTime_6 = 0;
+long lastDebounceTime_7 = 0;
+long lastDebounceTime_8 = 0;
+
+long lastDebounceTime = 0;
+
+
 long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 int allow_button = 1; //can just press button with no button pressed
 
 long time_waiting = 0; //the time passed until this point.
 
-long waiting_order_time = 20000; //Time to player select if he wants to start playing.
+long waiting_order_time = 5000; //Time to player select if he wants to start playing.
 
 void waiting_order();
 
@@ -83,29 +119,41 @@ void turn_on_led(int ledPin, int state) {
   digitalWrite(ledPin, state);
 }
 
+int check_buttons() {
+  int reading_0 = digitalRead(buttonPin_0), reading_1 = digitalRead(buttonPin_1),
+  reading_2 = digitalRead(buttonPin_2), reading_3 = digitalRead(buttonPin_3),
+  reading_4 = digitalRead(buttonPin_4), reading_5 = digitalRead(buttonPin_5),
+  reading_6 = digitalRead(buttonPin_6), reading_7 = digitalRead(buttonPin_7),
+  reading_8 = digitalRead(buttonPin_8);
+  int buttons =  (reading_0 + reading_1 + reading_2 + reading_3 + reading_4 + reading_5 + reading_6 + reading_7 + reading_8) ;
+  Serial.println(buttons);
+  return buttons == 8;
+}
+
+
 /**
  * Decouncing routine, It only works if the button was kept pressed for more than debounceDelay, if it changes the state of a variable 
  * no other button can be pressed while the button is still pressed.
  */
-void debounce_button(const int buttonPin, long *lastDebounceTime, long debounceDelay, int *buttonState, int *lastButtonState, void(*action)()) {
+Pos debounce_button(const int buttonPin, long *lastDebounceTime, long debounceDelay, int *buttonState, int *lastButtonState) {
   int reading = digitalRead(buttonPin);
-  if (allow_button) {
-    if (reading != *lastButtonState) {
-      *lastDebounceTime = millis();
-    }
+  if (reading != *lastButtonState) {
+    *lastDebounceTime = millis();
+  }
 
-    if ((millis() - *lastDebounceTime) > debounceDelay) {
-      if (reading != *buttonState && allow_button) {
-        *buttonState = reading;
-        // only toggle the LED if the new button state is HIGH
-        if (*buttonState == HIGH) {
-          allow_button = false;
-          action();
-        }
+  if ((millis() - *lastDebounceTime) > debounceDelay) {
+    if (reading != *buttonState) {
+      *buttonState = reading;
+      // only toggle the LED if the new button state is HIGH
+      if (*buttonState == HIGH) {
+        allow_button = false;
+        int real_pos[2] = {buttonPin_to_board[buttonPin - 2][0], buttonPin_to_board[buttonPin - 2][1]};
+        return create_position(real_pos[0], real_pos[1]);
       }
     }
-    *lastButtonState = reading;
   }
+  *lastButtonState = reading;
+  return create_position(-1, -1);
 }
 
 
@@ -171,6 +219,19 @@ Pos_weight create_Pos_weight(int x, int y, int weight) {
   return pos;
 }
 
+void update_leds();
+void update_leds(int board[][3]);
+
+void print_next_movements(Pos_vector_zsize vec){
+  int i;
+  for(i = 0; i < vec.size_vector; ++i){
+    Serial.print(vec.pos[i].x);
+    Serial.print(" ");
+    Serial.print(vec.pos[i].y);
+  }
+  Serial.println();
+}
+
 /**
  * Creates the AI next possible movements to use in the min max.
  */
@@ -189,7 +250,7 @@ Pos_vector_zsize get_next_position(int board[][3]) {
   for (i = 0; i < 3; ++i) {
     for (j = 0; j < 3; ++j) {
       if (!board[i][j]) {
-        vec.pos[0] = create_position(i, j);
+        vec.pos[counter] = create_position(i, j);
         counter++;
       }
     }
@@ -198,48 +259,82 @@ Pos_vector_zsize get_next_position(int board[][3]) {
   return vec;
 }
 
+
 Pos read_player_movement() {
-  //TODO
+  Pos pos = debounce_button(buttonPin_0, &lastDebounceTime_0, debounceDelay, &buttonState_0, &lastButtonState_0);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_1, &lastDebounceTime_1, debounceDelay, &buttonState_1, &lastButtonState_1);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_2, &lastDebounceTime_2, debounceDelay, &buttonState_2, &lastButtonState_2);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_3, &lastDebounceTime_3, debounceDelay, &buttonState_3, &lastButtonState_3);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_4, &lastDebounceTime_4, debounceDelay, &buttonState_4, &lastButtonState_4);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_5, &lastDebounceTime_5, debounceDelay, &buttonState_5, &lastButtonState_5);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_6, &lastDebounceTime_6, debounceDelay, &buttonState_6, &lastButtonState_6);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_7, &lastDebounceTime_7, debounceDelay, &buttonState_7, &lastButtonState_7);
+  if(pos.x > -1)return pos;
+  pos = debounce_button(buttonPin_8, &lastDebounceTime_8, debounceDelay, &buttonState_8, &lastButtonState_8);
+  return pos;
 }
 
-void write_player_movement(Pos posit) {
-  //TODO
+void write_player_movement(Pos posit, int player_id) {
+  //Stub
+  board[posit.x][posit.y] = player_id;
 }
 
 
 /**
  * Check row and colun at the same time to save processing time
  */
-bool check_row_column() {
+int check_row_column(int board[][3]) {
   int i, j;
   for (i = 0; i < 3; ++i) {
-    int sum_row = 0;
-    int sum_column = 0;
+    int sum_row_1 = 0;
+    int sum_row_2 = 0;
+    int sum_column_1 = 0;
+    int sum_column_2 = 0;
     for (j = 0; j < 3; ++j) {
-      sum_row += board[i][j];
-      sum_column += board[j][i];
+      sum_row_1 += (board[i][j] == 1);
+      sum_column_1 += (board[j][i] == 1);
+      sum_row_2 += (board[i][j] == 2);
+      sum_column_2 += (board[j][i] == 2);
     }
-    if (sum_row == 3) return true;
-    if (sum_column == 3) return true;
+    if (sum_row_1 == 3) return 1;
+    if (sum_column_1 == 3) return 1;
+    if (sum_row_2 == 3) return 3;
+    if (sum_column_2 == 3) return 3;
   }
-  return false;
+  return 0;
 }
 
-bool check_diagonal() {
+int check_diagonal(int board[][3]) {
   int i, j;
-  int sum_main = 0;
-  int sum_opo = 0;
+  int sum_main_1 = 0;
+  int sum_opo_1 = 0;
+  int sum_main_2 = 0;
+  int sum_opo_2 = 0;
   for (i = 0; i < 3; ++i) {
-    sum_main += board[i][i];
-    sum_opo += board[2 - i][i];
+    sum_main_1 += (board[i][i] == 1);
+    sum_opo_1 += (board[2 - i][i] == 1);
+    sum_main_2 += (board[i][i] == 2);
+    sum_opo_2 += (board[2 - i][i] == 2);
   }
-  if (sum_main == 3) return true;
-  if (sum_opo == 3) return true;
-  return false;
+  if (sum_main_1 == 3) return 1;
+  if (sum_opo_1 == 3) return 1;
+  if (sum_main_2 == 3) return 3;
+  if (sum_opo_2 == 3) return 3;
+  return 0;
 }
 
-bool check_game_over() {
-  return check_row_column() || check_diagonal();
+int check_game_over(int board[][3]) {
+  int result = check_row_column(board) + check_diagonal(board);
+  if(result == 0) return 0;
+  if(result%3 == 0) return -1;
+  return 1;
 }
 
 /**
@@ -249,6 +344,10 @@ int min_alpha_beta(int board[][3], int alpha, int beta);
 
 int max_alpha_beta(int board[][3], int alpha, int beta) {
   int best_value = alpha;
+  int answer = check_game_over(board);
+  if(answer != 0){
+    return answer;
+  }
   Pos_vector_zsize next_movements = get_next_position(board);
   int i;
   for (i = 0; i < next_movements.size_vector; ++i) {
@@ -256,48 +355,72 @@ int max_alpha_beta(int board[][3], int alpha, int beta) {
     memcpy(copy,board, 9*sizeof(int));     
     copy[next_movements.pos[i].x][next_movements.pos[i].y] = 1;
     int value_from_child = min_alpha_beta(copy, best_value, beta);
+    //update_leds(copy); 
+    //Serial.println(value_from_child);
     best_value = max(value_from_child, best_value);
     if (best_value >= beta) {
       break;
     }
   }
   free(next_movements.pos);
+  if(next_movements.size_vector == 0) {
+    int answer = check_game_over(board);
+    return answer;
+  }
   return best_value;
 }
 
 int min_alpha_beta(int board[][3], int alpha, int beta) {
   int best_value = beta;
+  int answer = check_game_over(board);
+  if(answer != 0){
+    return answer;
+  }
   Pos_vector_zsize next_movements = get_next_position(board);
   int i;
   for (i = 0; i < next_movements.size_vector; ++i) {
     int copy[3][3];
     memcpy(copy,board, 9*sizeof(int));     
-    copy[next_movements.pos[i].x][next_movements.pos[i].y] = 1;
+    copy[next_movements.pos[i].x][next_movements.pos[i].y] = 2;
     int value_from_child = max_alpha_beta(copy, alpha, best_value);
+    //update_leds(copy); 
+    //Serial.println(value_from_child);
     best_value = min(value_from_child, best_value);
     if (best_value <= alpha) {
       break;
     }
   }
   free(next_movements.pos);
+  if(next_movements.size_vector == 0) {
+    int answer = check_game_over(board);
+    return answer;
+  }
   return best_value;
 }
 
 Pos search_next_position(){
   Pos next_position = {-1, -1};
   int alpha = -INFINITY, beta =  INFINITY;
+  //int board[][3] = {{0, 0, 0}, {0, 0, 1}, {2, 2, 1}};
   Pos_vector_zsize next_movements = get_next_position(board);
-  int i, best_value = INFINITY;
+  print_next_movements(next_movements);
+  int i, best_value = alpha;
   for (i = 0; i < next_movements.size_vector; ++i) {
     int copy[3][3];
     memcpy(copy,board, 9*sizeof(int));     
     copy[next_movements.pos[i].x][next_movements.pos[i].y] = 1;
-    int value_from_child = max_alpha_beta(copy, alpha, best_value);
-    if(value_from_child < best_value){
-      value_from_child = best_value;
+    int value_from_child = min_alpha_beta(copy, best_value, beta);
+    //update_leds(copy);  
+    //Serial.print(next_movements.pos[i].x);
+    //Serial.print(" ");
+    //Serial.print(next_movements.pos[i].y);
+    //Serial.print(" ");
+    //Serial.println(value_from_child);
+    if(value_from_child > best_value){
+      best_value = value_from_child;
       next_position = {next_movements.pos[i].x, next_movements.pos[i].y};
     }
-    if (best_value <= alpha) {
+    if (best_value >= beta) {
       break;
     }
   }
@@ -307,23 +430,37 @@ Pos search_next_position(){
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * Only allow a button to be pressed if no button is pressed
+ */
+
 void person_turn();
+
+void do_nothing(){};
 
 /**
  * We assumed that the AI will always generate a valid movement, so we just have to write it.
  */
 void computer_turn() {
+  Serial.println("Searching");
+  //update_leds();
   Pos next_movement = search_next_position();
-  write_player_movement(next_movement);
-  if (check_game_over()) {
+
+  write_player_movement(next_movement, 1);
+  update_leds();
+  Serial.println(next_movement.x);
+  Serial.println(next_movement.y);
+  delay(10000);
+  if (check_game_over(board)) {
     state = game_over;
   } else {
     state = person_turn;
+    //state = do_nothing;
   }
 }
 
 bool check_pos(Pos posit) {
-  return board[posit.x][posit.y] == 0;
+  return posit.x != -1 && board[posit.x][posit.y] == 0;
 }
 
 /**
@@ -333,11 +470,13 @@ bool check_pos(Pos posit) {
 void person_turn() {
   Pos pos = read_player_movement();
   bool valid_pos = check_pos(pos);
+  Serial.println(pos.x);
   if (valid_pos) {
-    write_player_movement(pos);
-    if (check_game_over()) {
+    write_player_movement(pos, 2);
+    if (check_game_over(board)) {
       state = game_over;
     } else {
+      delay(10000);
       state = computer_turn;
     }
   } else {
@@ -353,50 +492,70 @@ void selected_order() {
  * Wait until the user selects if he wants to be start the game.
  */
 void waiting_order() {
-  debounce_button(choose_order_pin, &lastDebounceTime, debounceDelay, &order_button_state, &last_order_state, selected_order);
+  Pos pos = debounce_button(choose_order_pin, &lastDebounceTime, debounceDelay, &order_button_state, &last_order_state);
+  if(pos.x == 3 && pos.y == 0){
+    state = person_turn;
+  }
   if ((millis() - time_waiting) > waiting_order_time) {
+    Serial.println("Computer turn.");
     state = computer_turn;
   }
 }
 
-/**
- * Only allow a button to be pressed if no button is pressed
- */
-void check_buttons() {
-  int reading_1 = digitalRead(buttonPin_1), reading_2 = digitalRead(buttonPin_2);
-  allow_button = !(reading_1 && reading_2);
-}
 
-}
-
-
-void setup() {
-  pinMode(buttonPin_1, INPUT);
-  pinMode(ledPin_1, OUTPUT);
-
-  // set initial LED_1 state
-  digitalWrite(ledPin_1, ledState_1);
-
-  pinMode(buttonPin_2, INPUT);
-  pinMode(ledPin_2, OUTPUT);
-
-  // set initial LED_2 state
-  digitalWrite(ledPin_2, ledState_2);
-  Serial.begin(9600);
-
-}
 
 /**
  * Responsible to turn on the leds of the board 3x3 with 1.
  */
 void update_leds(){
   //TODO
+  int i, j;
+  for(i = 0; i < 3; ++i){
+    for(j = 0; j < 3; ++j){
+      Serial.print("\t");
+      Serial.print(board[i][j]);
+    }
+    Serial.println();
+  }
+  Serial.println();
 }
+
+void update_leds(int board[][3]){
+  //TODO
+  int i, j;
+  for(i = 0; i < 3; ++i){
+    for(j = 0; j < 3; ++j){
+      Serial.print("\t");
+      Serial.print(board[i][j]);
+    }
+    Serial.println();
+  }
+  Serial.println();
+}
+
+}
+
+
+void setup() {
+  pinMode(buttonPin_0, INPUT);
+  pinMode(buttonPin_1, INPUT);
+  pinMode(buttonPin_2, INPUT);
+  pinMode(buttonPin_3, INPUT);
+  pinMode(buttonPin_4, INPUT);
+  pinMode(buttonPin_5, INPUT);
+  pinMode(buttonPin_6, INPUT);
+  pinMode(buttonPin_7, INPUT);
+  pinMode(buttonPin_8, INPUT);
+
+  Serial.begin(9600);
+  tic::time_waiting = millis();
+}
+
 
 void loop() {
   tic::check_buttons();
   tic::state();
-  update_leds();
+  tic::update_leds();
   //debounce(buttonPin_1, &lastDebounceTime, debounceDelay, &buttonState_1, &ledState_1, &lastButtonState_1, ledPin_1, turn_on_led);
   //debounce(buttonPin_2, &lastDebounceTime, debounceDelay, &buttonState_2, &ledState_2, &lastButtonState_2, ledPin_2, turn_on_led);
 
